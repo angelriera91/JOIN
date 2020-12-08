2493799365dac19e701636f860f9f7b2e0815a07
Juan pi Carpio <jpcarpio233@gmail.com>
2020-12-08 23:47:57 +0100

    poquito de tdo

--- join-app/src/app/app-routing.module.ts
+++ join-app/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 
 const routes: Routes = [
-  {path:"perfil", component:ProfileComponent}
+  {path:"perfil", component:ProfileComponent},
+  {path:"**",component:HomeComponent}
 ];
 
 @NgModule({
