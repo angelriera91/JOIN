0695b0915fc59fdf8494ceca439b133dd0f22587
angelriera91 <angelriera91@gmail.com>
2020-12-10 20:57:16 +0100

    create componente events, y modal de event asi como modal de create event

--- join-app/src/app/pages/events/events.component.ts
+++ join-app/src/app/pages/events/events.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Event } from '../../model/event/event'
 
 @Component({
   selector: 'app-events',
@@ -7,7 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventsComponent implements OnInit {
 
-  constructor() { }
+  closeResult = '';
+  public event: Event = new Event()
+
+  constructor(private modalService: NgbModal) {
+    this.event
+  }
+
+  public open(eventmodal) {
+    this.modalService.open(eventmodal, {backdropClass: 'light-blue-backdrop'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+  
+  ;
+  
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
 
   ngOnInit(): void {
   }
