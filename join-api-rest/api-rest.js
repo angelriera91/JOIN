94b2523eed65f6977d4fe82b743cfa57886e85bd
Jfaramir <jpcarpio233@gmail.com>
2020-12-16 16:44:39 +0100

    ajajaj que gracioso

--- join-api-rest/api-rest.js
+++ join-api-rest/api-rest.js
@@ -95,35 +95,37 @@ app.get("/event/bycreator"/* /event/bycreator/:id */, function(request,response)
     var f = new Date();
     var fecha = f.getFullYear() + '-' + (f.getMonth() + 1) + '-' + f.getDate();
     var params = [request.body.id_creador,fecha]
-    console.log(fecha);
 
     let sql = 'SELECT * FROM eventos WHERE id_creador = ? AND fecha >= "?"';
     
-    connection.query(sql,function(err,result){
+    connection.query(sql,params,function(err,result){
         if (err) {
+            response.send(err);
             console.log(err);
         } else {
             response.send(result);
-            console.log(result);    
+            console.log(result);
         }
     })
+
 });
 
 // usuarios favoritos -JP
 app.get("/user/favorito"/* /user/favorito/:id */,function(request,response) {
-    var params = [request.body.id_usuario]
 
+    var params = [request.body.id_usuario]
     let sql = 'SELECT us.* FROM usuarios AS u INNER JOIN usuario_usuario AS uu ON u.id_usuario = uu.id_usuario INNER JOIN usuarios AS us ON uu.id_seguidor = us.id_usuario WHERE uu.id_usuario = ? ORDER BY us.id_usuario'
-    console.log(sql);
     
-    connection.query(sql,function(err,result){
+    connection.query(sql,params,function(err,result){
         if (err) {
+            response.send(err);
             console.log(err);
         } else {
             console.log(result);
             response.send(result);
         }
     })
+
 });
 
 
@@ -132,20 +134,20 @@ app.get("/user/favorito"/* /user/favorito/:id */,function(request,response) {
 app.get("/event/pasados"/* /event/pasados/:id */, function(request,response) {
     var f = new Date();
     var fecha = f.getFullYear() + '-' + (f.getMonth() + 1) + '-' + f.getDate();
-    var params = [request.body.id_usuario,fecha]
-    console.log(fecha);
+    var params = [request.body.id_usuario,fecha];
 
     let sql = 'SELECT e.* FROM usuarios AS u INNER JOIN usuario_eventos AS ue ON u.id_usuario = ue.id_usuario INNER JOIN eventos AS e ON ue.id_evento = e.id_event WHERE ue.id_usuario = ? AND e.fecha <= "?" ORDER BY e.fecha DESC';
-    console.log(sql);
 
-    connection.query(sql,function(err,result){
+    connection.query(sql,params,function(err,result){
         if (err) {
+            response.send(err);
             console.log(err);
         } else {
             response.send(result);
             console.log(result);
         }
     })
+
 });
 
 
@@ -154,20 +156,20 @@ app.get("/event/pasados"/* /event/pasados/:id */, function(request,response) {
 app.get("/event/asistir"/* /event/asistir/:id */, function(request,response) {
     var f = new Date();
     var fecha = f.getFullYear() + '-' + (f.getMonth() + 1) + '-' + f.getDate();
-    var params = [request.body.id_usuario,fecha]
-    console.log(fecha);
+    var params = [request.body.id_usuario,fecha];
 
     let sql = 'SELECT e.* FROM usuarios AS u INNER JOIN usuario_eventos AS ue ON u.id_usuario = ue.id_usuario INNER JOIN eventos AS e ON ue.id_evento = e.id_event WHERE ue.id_usuario = ? AND e.fecha >= "?" ORDER BY e.fecha ASC';
-    console.log(sql);
 
     connection.query(sql,params,function(err,result){
         if (err) {
+            response.send(err);
             console.log(err);
         } else {
             response.send(result);
             console.log(result);
         }
     })
+
 });
 
 
