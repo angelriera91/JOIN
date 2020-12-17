1932acf85b3414e9a9574e69598b168a2fe844dc
angelriera91 <angelriera91@gmail.com>
2020-12-17 20:30:30 +0100

    funcionalidad mostrar eventos working

--- join-api-rest/api-rest.js
+++ join-api-rest/api-rest.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const cors = require('cors');
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
